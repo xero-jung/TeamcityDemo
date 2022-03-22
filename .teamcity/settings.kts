1f594a1a819e718cd1991b9c427b93291c557b42
jung.woo <jung.woo@TeamCity>
2022-03-22 00:45:19 +0000

    TeamCity change in 'Mobile / Platform / TeamcityDemo' project: build features of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.dockerSupport
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 /*
@@ -39,6 +40,14 @@ object Build : BuildType({
 
     triggers {
         vcs {
+        }
+    }
+
+    features {
+        dockerSupport {
+            loginToRegistry = on {
+                dockerRegistryId = "PROJECT_EXT_200"
+            }
         }
     }
 
